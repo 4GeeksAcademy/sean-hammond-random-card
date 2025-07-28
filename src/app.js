44d6d2d865b1fc9fa337c0c62be5463aaba3fbec
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-07-28 22:50:01 +0000

    updated with array of card values

--- src/app.js
+++ src/app.js
@@ -10,10 +10,12 @@ window.onload = function() {
 
   const randomDec = Math.random();
   
-  const randomLongNum = random * 5;
+  const randomLongNum = random * MyArray.length;
 
   const randomNumNoDec = Math.floor(randomLongNum);
   //removed decimals
 
-  div.innerHTML = randomNumNoDec;
+  const myArray = ["A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K"];
+
+  div.innerHTML = myArray[randomNumNoDec];
 };
