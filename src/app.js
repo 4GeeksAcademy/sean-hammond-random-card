424a659d8a532f5343d4a01046692d10bac2caac
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-07-28 23:49:21 +0000

    added random suit generator - note: divs do not show up on browser, but everything else does

--- src/app.js
+++ src/app.js
@@ -6,16 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  const div = document.querySelector("#card");
+
+  valueDiv.addEventListener("mouseover", drawCard());
+  valueDiv.addEventListener("mouseout", ()=> div.style.color = "green");
+
+  console.log("beginning of code");
+  const cardDiv = document.querySelector("#card");
+  const suitDiv = document.querySelector("#card");
 
   const randomDec = Math.random();
   
-  const randomLongNum = random * MyArray.length;
+  const randomLongNum = randomDec * valueArray.length;
 
   const randomNumNoDec = Math.floor(randomLongNum);
   //removed decimals
 
-  const myArray = ["A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K"];
 
-  div.innerHTML = myArray[randomNumNoDec];
+  
+  const valueArray = ["A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K"];
+  const suitArray = ["heart❤︎", "spade♠️", "club♣️", "diamond◆"];
+
+  cardDiv.innerHTML = valueArray[randomNumNoDec];
+  suitDiv.innerHTML = valueArray[randomNumNoDec];
+
+  console.log("random card value should be:", div.innerHTML = valueArray[randomNumNoDec]);
+};
+
+const drawCard = ()=> {
+  console.log("user clicked draw card button");
 };
