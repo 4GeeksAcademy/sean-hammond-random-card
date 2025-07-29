4aed88d88cdd3eb1ab249eec336ab5ec335b811c
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-07-29 00:37:14 +0000

    added random number generators for suit

--- src/app.js
+++ src/app.js
@@ -4,34 +4,41 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
 window.onload = function() {
+    const drawArea = document.querySelector("#drawButton");
+  drawArea.addEventListener("click", drawCard);
+  // suitDivDiv.addEventListener("mouseout", ()=> div.style.color = "green");
 
-  valueDiv.addEventListener("mouseover", drawCard());
-  valueDiv.addEventListener("mouseout", ()=> div.style.color = "green");
-
+  
+  
   console.log("beginning of code");
-  const cardDiv = document.querySelector("#card");
-  const suitDiv = document.querySelector("#card");
+  const valueDiv = document.querySelector("#card"); // number on face of card
+  const suitDiv = document.querySelector("#suit");
 
-  const randomDec = Math.random();
-  
-  const randomLongNum = randomDec * valueArray.length;
-
-  const randomNumNoDec = Math.floor(randomLongNum);
-  //removed decimals
-
-
-  
   const valueArray = ["A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K"];
   const suitArray = ["heart❤︎", "spade♠️", "club♣️", "diamond◆"];
 
-  cardDiv.innerHTML = valueArray[randomNumNoDec];
-  suitDiv.innerHTML = valueArray[randomNumNoDec];
+  const randomDecForValue = Math.random();
+  const randomDecForSuit = Math.random();
+  
+  const valueNum = randomDecForValue * valueArray.length;
+  const suitNum = randomDecForSuit * suitArray.length;
 
-  console.log("random card value should be:", div.innerHTML = valueArray[randomNumNoDec]);
+
+  
+
+  const randomNumNoDecValue = Math.floor(valueNum);
+  const randomNumNoDecSuit = Math.floor(suitNum);
+  //removed decimals
+
+  valueDiv.innerHTML = valueArray[randomNumNoDecValue];
+  suitDiv.innerHTML = suitArray[randomNumNoDecSuit];
+
 };
 
 const drawCard = ()=> {
+  
   console.log("user clicked draw card button");
 };
